8e93e6a42c6996c2ae98bc02c315df6d27fda61e
loonylunar1324 <loonylunar1324@gmail.com>
2019-10-30 23:24:07 +0900

    Uploaded Project

---
+++ cultureland/cultureland/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 307)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(348, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "생성"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(676, 249)
+        Me.TextBox1.TabIndex = 1
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(12, 285)
+        Me.HScrollBar1.Maximum = 1009
+        Me.HScrollBar1.Minimum = 10
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(676, 19)
+        Me.HScrollBar1.TabIndex = 2
+        Me.HScrollBar1.Value = 10
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(366, 307)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(322, 41)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "삭제"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 267)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "생성 개수 : 10"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(700, 360)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Cultureland Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ cultureland/cultureland/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim random As New Random
+        Dim A, B, C, D
+        Dim i
+        For i = 1 To HScrollBar1.Value
+            A = random.Next(1000, 9999)
+            B = random.Next(1000, 9999)
+            C = random.Next(1000, 9999)
+            D = random.Next(100000, 999999)
+            TextBox1.AppendText(A & "-" & B & "-" & C & "-" & D & Environment.NewLine)
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+    End Sub
+    Private Sub HScrollBar1_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar1.Scroll
+        Label1.Text = "생성 개수 : " & HScrollBar1.Value
+    End Sub
+End Class
